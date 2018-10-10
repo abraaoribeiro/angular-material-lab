e418cdc2055bff24cbb4d48b0663329b0a125c4c
Abraão Souza Ribeiro <abraao.ribeiro@tre-pa.jus.br>
2018-10-10 11:25:21 -0300

    commit

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatButtonModule} from '@angular/material';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
     declarations: [],
-    imports: [ CommonModule ,MatButtonModule,MatSidenavModule,MatMenuModule,MatToolbarModule],
-    exports: [CommonModule ,MatButtonModule,MatSidenavModule,MatMenuModule,MatToolbarModule],
+    imports: [CommonModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule],
+
+    exports: [CommonModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule],
     providers: [],
 })
-export class AppMaterialModule {}
+export class AppMaterialModule { }
